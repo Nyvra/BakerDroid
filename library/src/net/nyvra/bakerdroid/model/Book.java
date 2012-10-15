5e26491c402145330c89dbb36fa4e34a2772260a
castelanjr <angelo.castelanjr@gmail.com>
2012-10-15 15:43:46 -0300

    Add a constructor to load book from assets.

--- library/src/net/nyvra/bakerdroid/model/Book.java
+++ library/src/net/nyvra/bakerdroid/model/Book.java
@@ -1,8 +1,24 @@
 package net.nyvra.bakerdroid.model;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringWriter;
+import java.io.Writer;
+import java.util.ArrayList;
 import java.util.List;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.content.res.AssetManager;
+
 public class Book {
+	private String mPath;
 	private String mTitle;
 	private String[] mAuthor;
 	private String mUrl;
@@ -16,6 +32,46 @@ public class Book {
 	private String mCover;
 	private String mFile;
 	private List<Setting> mSettings;
+	
+	
+	//NOTE: just a test
+	public Book(Context context, String path) {
+		AssetManager assetManager = context.getAssets();
+		Writer writer = new StringWriter();
+		char[] buffer = new char[1024];
+		try {
+			InputStream input = assetManager.open(path.concat("/book.json"));
+			Reader reader = new BufferedReader(new InputStreamReader(input, "UTF-8"));
+			int n;
+			while ((n = reader.read(buffer)) != -1) {
+				writer.write(buffer, 0, n);
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		try {
+			JSONObject object = new JSONObject(writer.toString());
+			this.mTitle = object.getString("title");
+			this.mUrl = object.getString("url");
+			this.mPath = path;
+			
+			JSONArray authorArray = object.getJSONArray("author");
+			this.mAuthor = new String[authorArray.length()];
+			for (int i = 0; i < authorArray.length(); i++) {
+				this.mAuthor[i] = authorArray.getString(i);
+			}
+			
+			this.mContent = new ArrayList<String>();
+			JSONArray contentArray = object.getJSONArray("contents");
+			for (int i = 0; i < contentArray.length(); i++) {
+				this.mContent.add(contentArray.getString(i));
+			}
+		} catch (JSONException e) {
+			e.printStackTrace();
+		}
+	}
 
 	public String getTitle() {
 		return mTitle;
@@ -119,6 +175,18 @@ public class Book {
 
 	public void setSettings(List<Setting> mSettings) {
 		this.mSettings = mSettings;
+	}
+
+	public String getPath() {
+		return mPath;
+	}
+
+	public void setPath(String mPath) {
+		this.mPath = mPath;
+	}
+	
+	public String getUrlAtPosition(int position) {
+		return "file:///android_asset/".concat(this.getPath()).concat("/").concat(this.getContent().get(position));
 	}
 
 }
