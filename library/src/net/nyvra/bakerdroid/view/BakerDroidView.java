f3540ef2684ad996748ebac544f2f95df1d9ff41
castelanjr <angelo.castelanjr@gmail.com>
2012-10-15 16:46:53 -0300

    Handle index clicks.

--- library/src/net/nyvra/bakerdroid/view/BakerDroidView.java
+++ library/src/net/nyvra/bakerdroid/view/BakerDroidView.java
@@ -2,14 +2,20 @@ package net.nyvra.bakerdroid.view;
 
 import net.nyvra.bakerdroid.R;
 import net.nyvra.bakerdroid.model.Book;
+import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
+import android.webkit.WebViewClient;
 
 public class BakerDroidView extends ViewPager {
 	private Book mBook;
@@ -35,13 +41,21 @@ public class BakerDroidView extends ViewPager {
 	
 	class BakerDroidAdapter extends PagerAdapter {
 		
+		@SuppressLint({ "SetJavaScriptEnabled", "NewApi" })
 		@Override
 		public Object instantiateItem(ViewGroup container, int position) {
 			WebView webView = (WebView) LayoutInflater.from(mContext).inflate(R.layout.webview, null);
-			webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
 			webView.getSettings().setBuiltInZoomControls(true);
-			webView.loadUrl(mBook.getUrlAtPosition(position));
+			if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+				webView.getSettings().setDisplayZoomControls(false);
+			}
+			webView.getSettings().setJavaScriptEnabled(true);
+			webView.getSettings().setLoadWithOverviewMode(false);
+			webView.getSettings().setUseWideViewPort(false);
+			webView.setScrollBarStyle(WebView.SCROLLBARS_OUTSIDE_OVERLAY);
 			webView.setInitialScale(100);
+			webView.setWebViewClient(new BakerWebViewClient());
+			webView.loadUrl(mBook.getUrlAtPosition(position));
 			container.addView(webView);
 			return webView;
 		}
@@ -61,6 +75,20 @@ public class BakerDroidView extends ViewPager {
 			return view == object;
 		}
 		
+	}
+	
+	private class BakerWebViewClient extends WebViewClient {
+		@Override
+	    public boolean shouldOverrideUrlLoading(WebView view, String url) {
+			Log.d("--------------hrhrr", url);
+			int position = mBook.getPositionFromPage(url);
+	        if (position != -1) {
+	            BakerDroidView.this.setCurrentItem(position, true);
+	            return true;
+	        } else {
+	        	return false;
+	        }
+	    }
 	}
 
 }
