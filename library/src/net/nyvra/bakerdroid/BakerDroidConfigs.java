f999947d667a0484c3ab5b9ecef8263b3248e4a9
castelanjr <angelo.castelanjr@gmail.com>
2012-10-19 13:39:33 -0300

    Update Configs as BakerDroidConfigs.

--- library/src/net/nyvra/bakerdroid/BakerDroidConfigs.java
+++ library/src/net/nyvra/bakerdroid/BakerDroidConfigs.java
@@ -1,6 +1,6 @@
 package net.nyvra.bakerdroid;
 
-public class Configs {
+public class BakerDroidConfigs {
 	public enum StorageMode {STORAGE_ASSETS_FOLDER, STORAGE_EXTERNAL}
 	
 	public static StorageMode sStorageMode = StorageMode.STORAGE_ASSETS_FOLDER;
