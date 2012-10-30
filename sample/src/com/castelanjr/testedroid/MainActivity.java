f6981b52ae97b9da6b70b1c77915aab0a97e6413
castelanjr <angelo.castelanjr@gmail.com>
2012-10-30 10:45:54 -0200

    Fix sample.

--- sample/src/com/castelanjr/testedroid/MainActivity.java
+++ sample/src/com/castelanjr/testedroid/MainActivity.java
@@ -14,7 +14,7 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         
         mBaker = (BakerDroidView) findViewById(R.id.baker);
-        mBaker.loadDocument("book");
+        mBaker.loadDocument("book", null, 0);
     }
 
     @Override
