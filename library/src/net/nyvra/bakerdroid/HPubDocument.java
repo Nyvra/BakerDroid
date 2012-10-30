2f021d81140a588d74b63abbdee6e81d3acc0ecb
castelanjr <angelo.castelanjr@gmail.com>
2012-10-30 10:45:41 -0200

    Remove unnecessary setter methods.

--- library/src/net/nyvra/bakerdroid/HPubDocument.java
+++ library/src/net/nyvra/bakerdroid/HPubDocument.java
@@ -133,115 +133,58 @@ public class HPubDocument {
 		return mTitle;
 	}
 
-	public void setTitle(String mTitle) {
-		this.mTitle = mTitle;
-	}
-
 	public String[] getAuthor() {
 		return mAuthor;
-	}
-
-	public void setAuthor(String[] mAuthor) {
-		this.mAuthor = mAuthor;
 	}
 
 	public String getUrl() {
 		return mUrl;
 	}
 
-	public void setUrl(String mUrl) {
-		this.mUrl = mUrl;
-	}
-
 	public List<String> getContent() {
 		return mContent;
-	}
-
-	public void setContent(List<String> mContent) {
-		this.mContent = mContent;
 	}
 
 	public String getHPup() {
 		return mHpub;
 	}
 
-	public void setHPup(String mHPup) {
-		this.mHpub = mHPup;
-	}
-
 	public String[] getCreator() {
 		return mCreator;
-	}
-
-	public void setCreator(String[] mCreator) {
-		this.mCreator = mCreator;
 	}
 
 	public String getPublisher() {
 		return mPublisher;
 	}
 
-	public void setPublisher(String mPublisher) {
-		this.mPublisher = mPublisher;
-	}
-
 	public String getDate() {
 		return mDate;
-	}
-
-	public void setDate(String mDate) {
-		this.mDate = mDate;
 	}
 
 	public String getOrientation() {
 		return mOrientation;
 	}
 
-	public void setOrientation(String mOrientation) {
-		this.mOrientation = mOrientation;
-	}
-
 	public boolean isZoomable() {
 		return mZoomable;
-	}
-
-	public void setZoomable(boolean mZoomable) {
-		this.mZoomable = mZoomable;
 	}
 
 	public String getCover() {
 		return mCover;
 	}
 
-	public void setCover(String mCover) {
-		this.mCover = mCover;
-	}
-
 	public String getFile() {
 		return mFile;
-	}
-
-	public void setFile(String mFile) {
-		this.mFile = mFile;
 	}
 
 	public List<DocumentSetting> getSettings() {
 		return mSettings;
 	}
 
-	public void setSettings(List<DocumentSetting> mSettings) {
-		this.mSettings = mSettings;
-	}
-
 	public String getPath() {
 		return mPath;
 	}
-
-	public void setPath(String mPath) {
-		this.mPath = mPath;
-	}
 	
-	//TODO: get right path, not from assets
 	public String getUrlAtPosition(int position) {
 		if (BakerDroidConfigs.sStorageMode == StorageMode.STORAGE_ASSETS_FOLDER) {
 			return "file:///android_asset/".concat(this.getPath()).concat("/").concat(this.getContent().get(position));
