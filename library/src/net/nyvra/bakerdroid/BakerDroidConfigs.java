a15f68076e573b7379e8b8d22e84a42431b58a44
castelanjr <angelo.castelanjr@gmail.com>
2012-10-31 16:52:44 -0200

    Update BakerDroidConfigs - public methods and option to rewrite contents.

--- library/src/net/nyvra/bakerdroid/BakerDroidConfigs.java
+++ library/src/net/nyvra/bakerdroid/BakerDroidConfigs.java
@@ -3,6 +3,23 @@ package net.nyvra.bakerdroid;
 public class BakerDroidConfigs {
 	public enum StorageMode {STORAGE_ASSETS_FOLDER, STORAGE_EXTERNAL}
 	
-	public static StorageMode sStorageMode = StorageMode.STORAGE_ASSETS_FOLDER;
+	private static StorageMode sStorageMode = StorageMode.STORAGE_ASSETS_FOLDER;
+	private static boolean sOvewriteContentIfChanged = false;
+	
+	public static StorageMode getStorageMode() {
+		return sStorageMode;
+	}
+	
+	public static void setStorageMode(StorageMode sStorageMode) {
+		BakerDroidConfigs.sStorageMode = sStorageMode;
+	}
+	
+	public static boolean shouldOvewriteContentIfChanged() {
+		return sOvewriteContentIfChanged;
+	}
+	
+	public static void ovewriteContentIfChanged(boolean ovewriteContentIfChanged) {
+		BakerDroidConfigs.sOvewriteContentIfChanged = ovewriteContentIfChanged;
+	}
 
 }
